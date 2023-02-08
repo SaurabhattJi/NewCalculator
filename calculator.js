111fab8c7dbb99307cd2a46ef298e51a0b7e4f6c
Saurabh bhatt <120307815+SaurabhattJi@users.noreply.github.com>
2023-02-08 13:05:58 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+let string = "";
+let buttons= document.querySelectorAll("button");
+Array.from(buttons).forEach((button)=>{
+button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+        string = eval(string);
+        document.querySelector('input').value= string;  
+    }
+    else if(e.target.innerHTML == 'C'){
+        string = "";
+        document.querySelector('input').value= string;
+    }
+     else if(e.target.innerHTML == 'DEL'){
+        string = string.slice(0,-1);
+        document.querySelector('input').value= string;
+     }
+    else{
+        string = string + e.target.innerHTML;
+        document.querySelector('input').value= string;
+    }
+    })
+})
